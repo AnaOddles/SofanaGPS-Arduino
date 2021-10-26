aab63df52a5593855280c300aef231fe0fd7cfb9
Ana Sanchez <48583891+AnaOddles@users.noreply.github.com>
2021-10-26 00:20:40 -0700

    Added WifiLocation functionality for grabbing long and lat

--- SofanaGPS.ino/SofanaGPS.ino.ino
+++ SofanaGPS.ino/SofanaGPS.ino.ino
@@ -1,6 +1,55 @@
-void setup() {
-  // put your setup code here, to run once:
+#include <ESP8266WiFi.h> 
+#include <WifiLocation.h>
+//Import the wifi library
 
+const char* googleApiKey = "APIKEY";
+const char* ssid = "SSID";
+const char* passwd = "Password";
+WifiLocation location(googleApiKey);
+
+// Set time via NTP, as required for x.509 validation
+void setClock () {
+    configTime (0, 0, "pool.ntp.org", "time.nist.gov");
+
+    Serial.print ("Waiting for NTP time sync: ");
+    time_t now = time (nullptr);
+    while (now < 8 * 3600 * 2) {
+        delay (500);
+        Serial.print (".");
+        now = time (nullptr);
+    }
+    struct tm timeinfo;
+    gmtime_r (&now, &timeinfo);
+    Serial.print ("\n");
+    Serial.print ("Current time: ");
+    Serial.print (asctime (&timeinfo));
+}
+
+void setup() {
+ Serial.begin(115200);
+ Serial.println();
+ WiFi.mode(WIFI_STA);
+ WiFi.begin(ssid, passwd);
+
+ Serial.print("Connecting");
+ while(WiFi.status() != WL_CONNECTED)
+ {
+  delay(500);
+  Serial.println(".");
+  setClock();
+ }
+ Serial.println(); 
+ Serial.print("Connected, IP address: ");
+ Serial.println(WiFi.localIP());
+ WiFi.printDiag(Serial);
+
+ location_t loc = location.getGeoFromWiFi();
+ Serial.println("Location request data");
+ Serial.println(location.getSurroundingWiFiJson());
+ Serial.println("Latitude: " + String(loc.lat, 7));
+ Serial.println("Longitude: " + String(loc.lon, 7));
+ Serial.println("Accuracy: " + String(loc.accuracy));
+ Serial.println ("Result: " + location.wlStatusStr (location.getStatus ()));
 }
 
 void loop() {

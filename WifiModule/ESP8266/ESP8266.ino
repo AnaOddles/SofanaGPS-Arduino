895a3be05dfffcc6f8ceaf940abe26fd19e36af7
Ana Sanchez <48583891+AnaOddles@users.noreply.github.com>
2021-12-08 23:38:04 -0700

    Added wifi functionality

--- WifiModule/ESP8266/ESP8266.ino
+++ WifiModule/ESP8266/ESP8266.ino
@@ -1,13 +1,33 @@
 #include <SoftwareSerial.h>
+#include <ESP8266WiFi.h>
+#include <WifiLocation.h>
+
+const char* ssid = "ssid";
+const char* passwd = "password";
+
 void setup() {
-  //Open serial communication and wait for port to open: 
+  //Open serial communication and wait for port to open:
   Serial.begin(115200);
+
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, passwd); 
+
+  Serial.print("Connecting");
+  while (WiFi.status() != WL_CONNECTED)
+  {
+    delay(500);
+    Serial.println(".");
+  }
+  Serial.println();
+  Serial.print("Connected, IP address: ");
+  Serial.println(WiFi.localIP());
+  WiFi.printDiag(Serial);
 
 }
 
 void loop() {
-  if(Serial.available()){
+  if (Serial.available()) {
     Serial.write(Serial.read());
-    }
+  }
 
 }

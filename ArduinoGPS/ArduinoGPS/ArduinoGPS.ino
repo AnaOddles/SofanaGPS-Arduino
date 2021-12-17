c1ab63ff6a21b8220f5843b063f9ab784e91069b
Ana Sanchez <48583891+AnaOddles@users.noreply.github.com>
2021-12-16 23:17:21 -0700

    Communication between Arduino and ESP8266 with JSON Object and Serial Communication

--- ArduinoGPS/ArduinoGPS/ArduinoGPS.ino
+++ ArduinoGPS/ArduinoGPS/ArduinoGPS.ino
@@ -1,37 +1,50 @@
 #include <TinyGPS++.h>
 #include <SoftwareSerial.h>
-/*
-   This sample sketch demonstrates the normal use of a TinyGPS++ (TinyGPSPlus) object.
-   It requires the use of SoftwareSerial, and assumes that you have a
-   4800-baud serial GPS device hooked up on pins 4(rx) and 3(tx).
-*/
+#include <ArduinoJson.h> 
+
+//Global Variables
+
+//Serial Pins for GPS Module
 static const int RXPin = 4, TXPin = 3;
 static const uint32_t GPSBaud = 9600;
+
+//Storing the gps coordinates
+String longitude; 
+String latitude; 
 
 // The TinyGPS++ object
 TinyGPSPlus gps;
 
-// The serial connection to the GPS device
+//The serial connection to the GPS device
 SoftwareSerial ss(RXPin, TXPin);
 
 //The serial connection to the espmodule wifi board 
 SoftwareSerial espSerial(5,6); 
-String str; 
 
+//Function for configuring Arduino upon bootup
 void setup()
 {
+  //Start the Serial Monitor for debugging 
   Serial.begin(115200);
-  espSerial.begin(115200);
+  //Start the Serial connection for Wifi module
+  espSerial.begin(9600);
+  //Start the Serial connection for GPS module
   ss.begin(GPSBaud);
+
+  delay(1000); 
+  Serial.println("Program Started...\n");
 }
 
+//Function that runs when arduino is running - loops
 void loop()
 {
   // This sketch displays information every time a new sentence is correctly encoded.
   while (ss.available() > 0)
     if (gps.encode(ss.read()))
-      displayInfo();
-
+      //Call function to send data to Wifi Module
+      sendData();
+      
+  //If we are not receiving any encoding from GPS Module
   if (millis() > 5000 && gps.charsProcessed() < 10)
   {
     Serial.println(F("No GPS detected: check wiring."));
@@ -39,55 +52,53 @@ void loop()
   }
 }
 
-void displayInfo()
+//Function to send data to Wifi Module
+void sendData()
 {
+  //Setup json buffer that will store GPS coordinates as JSON
+  StaticJsonBuffer<1000> jsonBuffer; 
+  JsonObject& data = jsonBuffer.createObject(); 
+
+  //Grab long and lat coordinates 
+  grabCoordinates(); 
+
+  //Assign data to JSON objet 
+  data["lat"] = latitude; 
+  data["lon"] = longitude; 
+
+  //Send data to wifi module 
+  data.printTo(espSerial); 
+
+  //Clear the buffer for next coorindates 
+  jsonBuffer.clear(); 
+
+  //Wait 5 seconds
+  delay(5000); 
+  
+  Serial.println();
+}
+
+//Function that used to grab coorindates from the GPS Module
+void grabCoordinates(){
+  //If a valid gps location is grabbed
   if (gps.location.isValid())
   {
-    str = "Location: " + String(gps.location.lat(), 6) + " ," + String(gps.location.lng(), 6); 
+    //Grab a string of lat and lon for debugging
+    //str = "Location: " + String(gps.location.lat(), 6) + " ," + String(gps.location.lng(), 6); 
+    //espSerial.println(str); 
+
+    //Print to the Arduino Serial Monitor for debugging
     Serial.print(gps.location.lat(), 6);
     Serial.print(F(","));
     Serial.print(gps.location.lng(), 6);
-    espSerial.println(str); 
+
+    //Grab the coords 
+    longitude = String(gps.location.lng(), 6); 
+    latitude = String(gps.location.lat(), 6); 
+    
   }
   else
   {
     Serial.print(F("INVALID"));
-    espSerial.println("INVALID");
   }
-
-//  Serial.print(F("  Date/Time: "));
-//  if (gps.date.isValid())
-//  {
-//    Serial.print(gps.date.month());
-//    Serial.print(F("/"));
-//    Serial.print(gps.date.day());
-//    Serial.print(F("/"));
-//    Serial.print(gps.date.year());
-//  }
-//  else
-//  {
-//    Serial.print(F("INVALID"));
-//  }
-//
-//  Serial.print(F(" "));
-//  if (gps.time.isValid())
-//  {
-//    if (gps.time.hour() < 10) Serial.print(F("0"));
-//    Serial.print(gps.time.hour());
-//    Serial.print(F(":"));
-//    if (gps.time.minute() < 10) Serial.print(F("0"));
-//    Serial.print(gps.time.minute());
-//    Serial.print(F(":"));
-//    if (gps.time.second() < 10) Serial.print(F("0"));
-//    Serial.print(gps.time.second());
-//    Serial.print(F("."));
-//    if (gps.time.centisecond() < 10) Serial.print(F("0"));
-//    Serial.print(gps.time.centisecond());
-//  }
-//  else
-//  {
-//    Serial.print(F("INVALID"));
-//  }
-
-  Serial.println();
 }
--- WifiModule/ESP8266/ESP8266.ino
+++ WifiModule/ESP8266/ESP8266.ino
@@ -1,23 +1,83 @@
 #include <SoftwareSerial.h>
 #include <ESP8266WiFi.h>
 #include <WifiLocation.h>
+#include <ArduinoJson.h> 
 
+//Global Variables 
+
+//Store the lat and lon coorindates
+String lat; 
+String lon; 
+
+//Network Credentials
 const char* ssid = "ssid";
-const char* passwd = "password";
+const char* passwd = "pass";
 
+//The serial connection to the espmodule wifi board 
+//D6 = Rx & D5 = Tx
+SoftwareSerial nodemcu(D6, D5);
+
+//Function for configuring ESP8266 upon bootup
 void setup() {
   //Open serial communication and wait for port to open:
-  Serial.begin(115200);
+  Serial.begin(9600);
+  nodemcu.begin(9600);
+  
+  while (!Serial) continue;
+  
+  //Call function to connect to Wifi
+  connectToWifi();
+}
 
+//Function that runs when ESP8266 is running - loops
+void loop() {
+  //If Serial Communication from Arduino is successful
+  if (nodemcu.available()) {
+    Serial.println("Wifi Board recieving data\n"); 
+
+  //Setup Json buffer that will store GPS coordinates as JSON
+  StaticJsonBuffer<1000> jsonBuffer; 
+  //Parse the Json object passed from Arduino (GPS coorindates)
+  JsonObject& data = jsonBuffer.parseObject(nodemcu); 
+
+  //If error parsing - 
+  if (data == JsonObject::invalid()){
+    Serial.println("Invalid Json Object");
+    jsonBuffer.clear(); 
+    return; 
+    }
+
+   //Extract the coorindates from Json Object
+   lat = String(data["lat"]); 
+   lon = String(data["lon"]); 
+   
+  //Display coodinates for debugging
+  displayCoorindates(data);
+
+   delay(5000);
+  }
+  else{
+    Serial.println("Waiting for communication from GPS\n"); 
+    return;
+    }
+}
+
+//Function to connect system to Wifi network
+void connectToWifi(){
+  
+  //Start Wifi Connection
   WiFi.mode(WIFI_STA);
   WiFi.begin(ssid, passwd); 
-
   Serial.print("Connecting");
+
+  //Loop until we get a connection
   while (WiFi.status() != WL_CONNECTED)
   {
     delay(500);
     Serial.println(".");
   }
+
+  //Display connection information for debugging
   Serial.println();
   Serial.print("Connected, IP address: ");
   Serial.println(WiFi.localIP());
@@ -25,9 +85,17 @@ void setup() {
 
 }
 
-void loop() {
-  if (Serial.available()) {
-    Serial.write(Serial.read());
+//Function to display the GPS coordinates for debugging purposes
+void displayCoorindates(JsonObject& data){
+  //Sucessfully parsed Json Object 
+   Serial.println("______________________________________");
+   Serial.println("Json Object Recieved\n"); 
+   Serial.print("Recieved Latitude: "); 
+   Serial.println(lat); 
+   Serial.print("Recieved Longitude:  ");
+   Serial.println(lon); 
+   Serial.println("______________________________________");
   }
+ 
 
-}
+  
